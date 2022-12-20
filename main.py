5a6cf2ef96fcb15a44427922a50ddd192eab9f9e
engch <engchun@live.com.sg>
2022-12-20 21:01:34 +0800

    Update main.py (Version 1)
    1. fixed simple interest calculator; floating point input error
    2. added dynamic yearly interest calculator
    3. beautify CLI
    
    in mind to do next:
    1. add output to excel sheet for user to do charting if they wish to do so.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 """
-Compound interest calculator for monthly addition
-standard formula: Amount = Principal ( 1 + interest rate ) ^ years
-formula: standard formula but add same Principal after every year
+Compound interest calculator with yearly addition option
 """
+from os import system
+from sys import exit as sys_exit
+
+
+def clear():
+    """
+    clear screen
+    :return: none
+    """
+    system("cls||clear")
 
 
 def this_year_addition(principal, base):
@@ -16,7 +24,7 @@ def this_year_addition(principal, base):
     return new_base
 
 
-def compounded_interest(principal, interest):
+def compound_interest_gain(principal, interest):
     """
     get yearly interest gain of the new principal
     :param principal: yearly principal
@@ -43,70 +51,173 @@ def today_value_of_outcome(principal, year, inflation):
     :param principal: future value
     :param year: how many years to de-compound
     :param inflation: the inflation rate
-    :return:
+    :return: present value of future growth value
     """
     present_value = float(principal / pow(1 + inflation, year))
     return float("{:.2f}".format(present_value))
 
 
-def simple_cal():
+def simple_or_dynamic_interest_calculator_input():
+    """
+    same user input for simple/dynamic interest calculator
+    :return: base, interest, inflation
+    """
+    base = float("{:.2f}".format(float(input("Yearly Addition: $"))))
+    interest = float("{:.4f}".format(float(input("Interest in percentage: ")) / 100))
+    inflation = float("{:.4f}".format(float(input("Interest in inflation: ")) / 100))
+    return base, interest, inflation
+
+
+def simple_or_dynamic_interest_cal_output(total_contribution, inflation_principal, principal, this_year, year):
+    """
+    final output result for simple/complex interest calculator
+    :param total_contribution: total value inputted
+    :param inflation_principal: how total value would become if follow inflation rate
+    :param principal: how total value would become if follow interest
+    :param this_year: current year of result
+    :param year: total years
+    :return: print to screen
+    """
+    print(f"########## Year {this_year}/{year} result! ##########")
+    print(f"Total contribution: ${total_contribution}!")
+    print(f"Inflated value: ${inflation_principal}!")
+    print(f"Future value: ${principal}!")
+    if total_contribution != 0:
+        print(f"Gain vs Base {float('{:.2f}'.format(float(((principal / total_contribution) - 1)) * 100))}%")
+    else:
+        print("Gain vs Base: not relevant")
+    if inflation_principal != 0:
+        print(f"Gain vs Inflation {float('{:.2f}'.format(float(((principal / inflation_principal) - 1)) * 100))}%")
+    else:
+        print("Gain vs Inflation: not relevant")
+    print(f"############# END OF YEAR #############\n")
+
+
+def simple_compound_interest_calculator():
     """
     input yearly contribution with interest rate and inflation rate for a number of years.
     :return: total contribution, inflated value, future value, gain in percentage.
     """
+    """ user input parameter """
     principal = inflation_principal = 0
-    base = float("{:.2f}".format(float(input("Yearly Addition: $"))))
-    interest = float("{:.2f}".format(float(input("Interest in percentage: ")) / 100))
-    inflation = float("{:.2f}".format(float(input("Interest in inflation: ")) / 100))
     year = int(input("Years of holding: "))
+    base, interest, inflation = simple_or_dynamic_interest_calculator_input()
     total_contribution = base * year
-
+    """ calculation """
     for i in range(year):
-        """ interest gain """
+        """ simple interest gain """
         principal = this_year_addition(principal, base)
-        interest_gain = compounded_interest(principal, interest)
+        interest_gain = compound_interest_gain(principal, interest)
         principal = new_principal(principal, interest_gain)
-        """ inflation gain """
+        """ simple inflation gain """
         inflation_principal = this_year_addition(inflation_principal, base)
-        inflation_gain = compounded_interest(inflation_principal, inflation)
+        inflation_gain = compound_interest_gain(inflation_principal, inflation)
         inflation_principal = new_principal(inflation_principal, inflation_gain)
-
-    print(f"This is your total contribution: ${total_contribution}!")
-    print(f"This is the inflated value: ${inflation_principal}!")
-    print(f"This is the future value: ${principal}!")
-    print(f"Gain vs base {float('{:.2f}'.format((principal / total_contribution) - 1)) * 100}%")
-    print(f"Gain vs inflation {float('{:.2f}'.format((principal / inflation_principal) - 1)) * 100}%")
-    print("Thank you for using this CLI service :-)")
+    """ final output """
+    simple_or_dynamic_interest_cal_output(total_contribution, inflation_principal, principal, year, year)
 
 
-def complex_cal():
+def dynamic_compound_interest_calculator():
+    """
+    input yearly contribution with interest rate and inflation rate for a number of years dynamically.
+    :return: total contribution, inflated value, future value, gain in percentage. Yearly and final dynamically.
+    """
+    """ user input parameter part 1/2 """
     principal = inflation_principal = 0
     total_contribution = 0
     year = int(input("Years of holding: "))
+    this_year = 0
+    """ dynamic calculation """
     for i in range(year):
-        """ user input parameter """
-        base = float("{:.2f}".format(float(input("Addition of the year: $"))))
-        interest = float("{:.2f}".format(float(input("Interest in percentage: ")) / 100))
-        inflation = float("{:.2f}".format(float(input("Interest in inflation: ")) / 100))
+        """ user input parameter part 2/2 """
+        base, interest, inflation = simple_or_dynamic_interest_calculator_input()
         total_contribution += base
-
-        """ interest gain """
+        """ dynamic interest gain """
+        principal = this_year_addition(principal, base)
+        interest_gain = compound_interest_gain(principal, interest)
+        principal = new_principal(principal, interest_gain)
+        """ dynamic inflation gain """
+        inflation_principal = this_year_addition(inflation_principal, base)
+        inflation_gain = compound_interest_gain(inflation_principal, inflation)
+        inflation_principal = new_principal(inflation_principal, inflation_gain)
+        """ yearly output """
+        this_year += 1
+        simple_or_dynamic_interest_cal_output(total_contribution, inflation_principal, principal, this_year, year)
+    """ final output """
+    simple_or_dynamic_interest_cal_output(total_contribution, inflation_principal, principal, year, this_year)
 
 
 def main():
-    options = int(input("select\n1 for simple calculator\n2 for complex calculator\n--> "))
-    if options == 1:
-        simple_cal()
-    if options == 2:
-        pass
-    else:
-        main()
+    print("\n##############################################")
+    print("| Welcome to Engch interest calculator!      |")
+    print("| Only accept numerical numbers input.       |")
+    print("| Rounds off to nearest 2 significant value. |")
+    print("| Interest kicks in on year 1!               |")
+    print("##############################################\n")
+
+    print("Select your option!")
+    print("1: Simple compound interest calculator")
+    print("2: Dynamic compound interest calculator")
+    print("3: exit")
+
+    options = int(input("--> "))
+    clear()
+
+    """ get type of calculation to execute """
+    match options:
+        case 1:
+            simple_compound_interest_calculator()
+        case 2:
+            dynamic_compound_interest_calculator()
+        case 3:
+            sys_exit()
+        case _:
+            main()
 
 
 if __name__ == '__main__':
     while True:
         try:
             main()
+            print("Thank you for using this CLI service :-)")
             break
         except ValueError:
-            print("Invalid input, please enter numbers only")
+            print("Invalid input, please enter numerical numbers only.")
+        except OverflowError:
+            print("Numerical calculation too huge! Please scale it down and try again.")
+        except ZeroDivisionError:
+            print("OI funny ah.")
+        except FloatingPointError:
+            print("Computational error! Please try with lesser floating points.")
+        except BufferError:
+            print("Buffer error! Please restart and try again.")
+        except IndexError:
+            print("Index error! Sequence out of range.")
+        except KeyError:
+            print("Mapping error! Key not found in existing set.")
+        except ModuleNotFoundError:
+            print(ModuleNotFoundError)
+        except ImportError:
+            print(ImportError)
+        except KeyboardInterrupt:
+            print(KeyboardInterrupt)
+            sys_exit()
+        except MemoryError:
+            print(MemoryError)
+        except OSError:
+            print(OSError)
+        except RuntimeError:
+            print(RuntimeError)
+        except TabError:
+            print(TabError)
+        except IndentationError:
+            print(IndentationError)
+        except SyntaxError:
+            print(SyntaxError)
+        except SystemError:
+            print(SystemError)
+        except SystemExit:
+            print(SystemError)
+            sys_exit()
+        except TypeError:
+            print(TypeError)
